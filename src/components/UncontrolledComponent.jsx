c2c42adc9a8b79527c9e92b82844a7b0e85fa37a
RlxDn <colin12212004@gmail.com>
2021-11-08 09:41:30 +0900

    1108

--- src/components/UncontrolledComponent.jsx
+++ src/components/UncontrolledComponent.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 
 class UncontrolledComponent extends React.Component {
+
+  inputRef = React.createRef();
+
   render() {
+    console.log("init render", this.inputRef)
     return (
       <div>
-        <input id="my-input"/>
+        <input ref={this.inputRef}/>
         <button onClick={this.click}>전송하기</button>
       </div>
     );
   }
-
+  componentDidMount() {
+    console.log("componentDidMount", this.inputRef)
+  }
   click = () => {
-    const input = document.querySelector("#my-input");
-    console.log(input.value);
+    // const input = document.querySelector("#my-input");
+    // console.log(input.value);
+
+    console.log("(3)클릭ok : ", this.inputRef.current.value)
   }
 }
 export default UncontrolledComponent;
